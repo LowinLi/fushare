d01ab3ff129747361bc08acbed27efaee7dba0aa
Lowin <33021512+LowinLi@users.noreply.github.com>
2018-07-20 11:19:47 +0800

    Add files via upload

---
+++ fushare/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*- 
+
+__version__ = '1.0.0'
+__author__ = 'Lowin'
+
+
+"""
+大宗商品现货价格及基差
+"""
+from fushare.basis import (get_spotPrice_daily,
+                           get_spotPrice)
+
+
+"""
+期货持仓成交排名数据
+"""
+from fushare.cot import (get_rank_sum_daily,
+                         get_rank_sum,
+                         get_shfe_rank_table,
+                         get_czce_rank_table,
+                         get_dce_rank_table,
+                         get_cffex_rank_table)
+
+
+"""
+大宗商品仓单数据
+"""
+from fushare.receipt import (get_reciept)
+
+
+"""
+大宗商品仓单数据
+"""
+from fushare.rollYield import (get_rollYield_bar, get_rollYield)
