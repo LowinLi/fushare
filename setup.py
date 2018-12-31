10f8f00e211bc2586004747bc4a04a622cd89cf4
ZhenghangXie <38500767+ZhenghangXie@users.noreply.github.com>
2018-12-31 23:39:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
 		['pandas>=0.23.1',
 		'requests>=2.12.4',
 		'matplotlib>=2.0.0',
+		'lxml'
 		'bs4'],
     description='A utility for fundamentals data of China commodity futures',
 	packages = ['fushare'],
